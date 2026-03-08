87a0eaebb1129f4514a9603c8cc3318234b9940d
atomcorp <thomasmaxwellsmith@gmail.com>
2026-03-08 22:07:10 +0000

    svg struggle

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,20 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const rotationMatrix = (
+  centerX: number,
+  centerY: number,
+  x: number,
+  y: number,
+  radians: number
+) => {
+  const rotatedX =
+    centerX +
+    (x - centerX) * Math.cos(radians) -
+    (y - centerY) * Math.sin(radians);
+  const rotatedY =
+    centerY +
+    (x - centerX) * Math.sin(radians) +
+    (y - centerY) * Math.cos(radians);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  return { rotatedX, rotatedY };
+};
